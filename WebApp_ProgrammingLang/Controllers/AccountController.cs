96bf1d39976250244fc5aa7567aad4e4f0be01a4
GhostNurgat15 <nurgatin.marat2016@yandex.ru>
2022-05-30 12:58:28 +0500

    Переделано _Layout; Добавлено страница авторизации

---
+++ WebApp_ProgrammingLang/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_ProgrammingLang.Controllers
+{
+    using Models;
+
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login() => View();
+    }
+}
