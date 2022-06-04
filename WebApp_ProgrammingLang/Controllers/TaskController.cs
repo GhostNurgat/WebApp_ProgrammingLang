a37e5ac51dac6fbbf7dce0939ab3f7ad1930cd60
GhostNurgat15 <nurgatin.marat2016@yandex.ru>
2022-06-04 15:57:00 +0500

    Реализовано поиск и фильтрация по типу языка

--- WebApp_ProgrammingLang/Controllers/TaskController.cs
+++ WebApp_ProgrammingLang/Controllers/TaskController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace WebApp_ProgrammingLang.Controllers
 {
     using Models;
+    using ViewModels;
     using Data;
 
     public class TaskController : Controller
@@ -17,10 +19,27 @@ namespace WebApp_ProgrammingLang.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> Language()
+        public async Task<IActionResult> Language(string searchLang, string typeString)
         {
-            var langs = _context.ProgrammingLanguages.Include(l => l.Type);
-            return View(await langs.ToListAsync());
+            IQueryable<string> queryType = _context.ProgrammingLanguageTypes.Select(t => t.Title);
+
+            var langs = await _context.ProgrammingLanguages.Include(l => l.Type).ToListAsync();
+
+            if (!string.IsNullOrEmpty(searchLang))
+                langs = langs.Where(l => l.Title.Contains(searchLang)).ToList();
+
+            if (!string.IsNullOrEmpty(typeString))
+                langs = langs.Where(l => l.Type.Title == typeString).ToList();
+
+            var languageVM = new LanguagesViewModel
+            {
+                Languages = langs,
+                Types = new SelectList(await queryType.ToListAsync())
+            };
+
+            return View(languageVM);
         }
+
+
     }
 }
---
+++ WebApp_ProgrammingLang/ViewModels/LanguagesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WebApp_ProgrammingLang.ViewModels
+{
+    using Models;
+
+    public class LanguagesViewModel
+    {
+        public List<ProgrammingLanguage>? Languages { get; set; }
+        public SelectList? Types { get; set; }
+        public string? SearchLanguage { get; set; }
+        public string? StringType { get; set; }
+    }
+}
