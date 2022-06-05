2a032ee4e10e4116e11f2ee03f68ce0be172c888
GhostNurgat15 <nurgatin.marat2016@yandex.ru>
2022-06-05 12:27:17 +0500

    Добавлено страницы о разработке и сайте

--- WebApp_ProgrammingLang/Controllers/HomeController.cs
+++ WebApp_ProgrammingLang/Controllers/HomeController.cs
@@ -13,20 +13,16 @@ namespace WebApp_ProgrammingLang.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+        public IActionResult Index() => View();
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        public IActionResult Privacy() => View();
+
+        public IActionResult AboutDev() => View();
+
+        public IActionResult AboutSite() => View();
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        public IActionResult Error() =>
+            View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
