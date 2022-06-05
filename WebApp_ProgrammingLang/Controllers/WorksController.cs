61821eac57ff0a722fa449ac7655893b358c1ff6
GhostNurgat15 <nurgatin.marat2016@yandex.ru>
2022-06-05 16:27:48 +0500

    Добавлено страница с работами

---
+++ WebApp_ProgrammingLang/Controllers/WorksController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApp_ProgrammingLang.Controllers
+{
+    using Models;
+    using Data;
+
+    public class WorksController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
