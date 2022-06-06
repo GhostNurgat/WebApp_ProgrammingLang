52a62d14549b29bf0f0778da1cd714ddee142311
GhostNurgat15 <nurgatin.marat2016@yandex.ru>
2022-06-06 19:23:38 +0500

    Добавлено модальное окно заявки на становлении преподавателем

---
+++ WebApp_ProgrammingLang/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_ProgrammingLang.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Bid() => PartialView();
+    }
+}
--- WebApp_ProgrammingLang/wwwroot/js/site.js
+++ WebApp_ProgrammingLang/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(function () {
+    $.ajaxSetup({ cache: true });
+    $('#modalLink').click(function (e) {
+        e.preventDefault();
+        $.get(this.href, function (data) {
+            $('#modalContent').html(data);
+            $('#modalDialog').modal('show');
+        })
+    })
+})
