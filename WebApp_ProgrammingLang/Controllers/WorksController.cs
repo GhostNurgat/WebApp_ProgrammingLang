5504a4251b56b502f4e035a5d14bd1fa30fba0d1
GhostNurgat15 <nurgatin.marat2016@yandex.ru>
2022-06-10 10:37:28 +0500

    Добавлено страница редактировании работ

--- WebApp_ProgrammingLang/Controllers/WorksController.cs
+++ WebApp_ProgrammingLang/Controllers/WorksController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 
 namespace WebApp_ProgrammingLang.Controllers
@@ -15,6 +16,9 @@ namespace WebApp_ProgrammingLang.Controllers
         {
             _context = context;
         }
+
+        private bool WorkExists(int id)
+            => _context.Works.Any(w => w.ID == id);
 
         public async Task<IActionResult> Index()
         {
@@ -53,6 +57,61 @@ namespace WebApp_ProgrammingLang.Controllers
             }
 
             ViewBag.Work = new SelectList(await _context.ProgrammingLanguages.ToListAsync(), "ID", "Title");
+
+            return View(work);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> WorkDetail(int id)
+        {
+            var work = await _context.Works.Include(w => w.User)
+                .Include(w => w.ProgrammingLanguage).FirstOrDefaultAsync(w => w.ID == id);
+
+            if (work == null)
+                return NotFound();
+
+            ViewBag.Title = work.Title;
+
+            return View(work);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var work = _context.Works
+                .Include(w => w.ProgrammingLanguage).FirstOrDefaultAsync(w => w.ID == id);
+
+            if (work == null)
+                return NotFound();
+
+            ViewBag.Lang = new SelectList(await _context.ProgrammingLanguages.ToListAsync(), "ID", "Title");
+
+            return View(work);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Work work, int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            _context.Works.Update(work);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+                return RedirectToAction("WorkDetail", "Works", work.ID);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (WorkExists(work.ID))
+                    return NotFound();
+                else
+                    throw;
+            }
 
             return View(work);
         }
